fbdccb112529117b613587571a01649539e55cc5
Ever <ieverx@outlook.com>
2016-05-15 17:31:04 +0800

    background: get old message and push to content script

--- extension/src/background/background.js
+++ extension/src/background/background.js
@@ -4,6 +4,8 @@
 const
     urlToTabIdsMap = new Map(),
     tabIdToUrl = new Map(),
+    urlMessages = new Map(),
+    urlClears = new Map(),
     MESSAGE_TAG = 'background';
 
 function removeTab(tabId) {
@@ -14,6 +16,10 @@ function removeTab(tabId) {
             if (urlToTabIdsMap.get(url).size === 0) {
                 window._SOCKET_HANDLER.leaveUrl(url);
                 urlToTabIdsMap.delete(url);
+                let intervalId = urlClears.get(url);
+                clearInterval(intervalId);
+                urlClears.delete(url);
+                urlMessages.delete(url);
             }
         }
     }
@@ -23,13 +29,10 @@ chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.tag === MESSAGE_TAG) return;
     if (message.tag === 'popup') {       
         delete message['tag'];
-        let tabIdSet = urlToTabIdsMap.get(message.url);
-        if (tabIdSet) {
-            tabIdSet.forEach(tabId => {
-                  chrome.tabs.sendMessage(tabId, message);
-            });
-            window._SOCKET_HANDLER.addMessage(message);
-        }
+        sendOneMessageToTab(message, msg => {
+            window._SOCKET_HANDLER.addMessage(msg)
+            urlMessages.get(msg.url).add(msg);
+        });
         return;
     } else { // page
         const
@@ -39,6 +42,14 @@ chrome.runtime.onMessage.addListener((message, sender) => {
         tabIdToUrl.set(tabId, url);
         if (!urlToTabIdsMap.has(url)) {
             window._SOCKET_HANDLER.openUrl(url);
+            window._SOCKET_HANDLER.getOldMessages(url).done(ms => {
+                if (ms) {
+                    urlMessages.set(url, ms);
+                } else {
+                    urlMessages.set(url, []);
+                }
+                pushMessageToTab(url);
+            });
             urlToTabIdsMap.set(url, new Set());
         }
         urlToTabIdsMap.get(url).add(tabId);
@@ -46,3 +57,28 @@ chrome.runtime.onMessage.addListener((message, sender) => {
 });
 
 chrome.tabs.onRemoved.addListener(removeTab);
+
+function sendOneMessageToTab(message, extra) {
+    let tabIdSet = urlToTabIdsMap.get(message.url);
+    if (tabIdSet) {
+        tabIdSet.forEach(tabId => {
+                chrome.tabs.sendMessage(tabId, message);
+        });
+        if (extra) {
+            extra(message);
+        }
+    }
+}
+
+function pushMessageToTab(url) {
+    if (!urlMessages.has(url)) {
+        let intervalId = setInterval(() => {
+            let msg = urlMessages.get(url).shift();
+            if (msg) {
+                sendOneMessageToTab(msg);
+                urlMessages.get(url).push(msg);
+            }
+        }, 1000);
+        urlClears.set(url, intervalId);
+    }
+}
