25db59c7991de85faf83aefd78483b48cfd3cd92
BrookShuihuaLee <759916638@qq.com>
2016-05-16 22:31:41 +0800

    飞慢点

--- extension/src/contentScripts/danMuKeYiYou.js
+++ extension/src/contentScripts/danMuKeYiYou.js
@@ -10,16 +10,15 @@ chrome.runtime.sendMessage({
 });
 
 chrome.runtime.onMessage.addListener((message, sender) => {
-    console.log(message, sender);
     if (!uids.has(message.uid)) {
         fly(message);
     }
-})
+});
 
 function fly(message) {
     uids.add(message.uid);    
-    var div = document.createElement('div');
-    div.className = 'dm--bullet'
+    let div = document.createElement('div');
+    div.className = 'dm--bullet';
     div.style.display = 'inline-block';
     div.style.whiteSpace = 'nowrap';
     div.style.color = message.color;
@@ -29,18 +28,23 @@ function fly(message) {
     div.style.zIndex =  '2147483647';
     document.body.insertBefore(div, null);
     div.style[message.direction] = `-${div.clientWidth}px`;
-    let top = (window.innerHeight - div.clientHeight)                
+    let top = (window.innerHeight - div.clientHeight);
     div.style.top = `${(Math.random() * top)}px`;
     div.style.display = '';
-    (function(direction, n) {
-        let intervalId = setInterval(move, 1);
+
+    (function(direction, duration) {
+        let startDate = new Date(),
+            intervalId = setInterval(move, 1);
         function move() {
-            if (parseFloat(div.style[direction]) >= window.innerWidth) {
-                clearInterval(intervalId)
+            let distance = div.clientWidth + window.innerWidth,
+                time = new Date() - startDate;
+            if (time > duration) {
+                clearInterval(intervalId);
+                div.remove();
                 uids.delete(message.uid);
             } else {
-                div.style[direction] = `${(parseFloat(div.style[direction]) + n)}px`;
+                div.style[direction] = `${time / duration * distance - div.clientWidth}px`;
             }
         }
-    })(message.direction, 0.5 + Math.random());
+    })(message.direction, 5000 + Math.random() * 5000);
 }
